589ed567c1ca492f8d7f57b9a17e5653d3c232f9
Mingfeng Li <lmf950122@gmail.com>
2021-06-04 01:44:12 +0000

    Implement with request-promise-native lib

---
+++ iss_promised.js
@@ -0,0 +1,24 @@
+const request = require("request-promise-native");
+
+
+const fetchMyIP = function() {
+  return request(`https://api.ipify.org?format=json`);
+};
+const fetchCoordsByIp = function (body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+const fetchISSFlyOverTimes = function(body){
+  const { latitude, longitude } = JSON.parse(body);
+  return request(`http://api.open-notify.org/iss/v1/?lat=${latitude}&lon=${longitude}`);
+};
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIp)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    })
+}
+module.exports = { nextISSTimesForMyLocation };
